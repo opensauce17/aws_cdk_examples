d647a6792aa512f2515ef556730a820dbcba2bc6
Michael Hyland <michael.hyland@kurtosys.com>
2021-08-03 14:36:38 +0200

    updated igore file and user-data

--- web_infrastructure/webserver_infra/user_data/user-data.sh
+++ web_infrastructure/webserver_infra/user_data/user-data.sh
@@ -3,4 +3,18 @@ yum update -y
 yum install -y httpd
 systemctl start httpd.service
 systemctl enable httpd.service
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+echo Hello World from $(hostname -f) > /var/www/html/index.html
+# Add apache logs to cloudwatch
+cat << 'EOF' >> /etc/awslogs/awslogs.conf
+[apache logs]
+datetime_format = %b %d %H:%M:%S
+file = /var/log/httpd/access_log
+buffer_duration = 5000
+log_stream_name = weblogs
+initial_position = start_of_file
+log_group_name = apaches_logs
+EOF
+sudo systemctl start awslogsd
+sudo chkconfig awslogsd on
+sudo systemctl enable awslogsd.service
+sudo curl localhost
