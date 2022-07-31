3d5a08db96226405e3c5fb25ad8f0f88b243d433
Mike Hyland <opensauce17@gmail.com>
2022-07-31 12:35:33 +0200

    updates

--- generic_resources/generic_resources/user_data/user-data.sh
+++ generic_resources/generic_resources/user_data/user-data.sh
@@ -2,7 +2,6 @@
 sudo cd /opt/registration_form_application && sudo git pull
 sudo chown www-data:www-data /opt/registration_form_application/*
 sudo systemctl restart reg-form.service
-sudo ls
 # sudo apt update
 # sudo apt install nginx
 # sudo systemctl enable nginx
